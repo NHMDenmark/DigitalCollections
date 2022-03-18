703c62874ff8a1071146cf12fe5bd8ec428f0e47
Kim <kimstp@di.ku.dk>
2022-03-18 13:44:00 +0100

    Adding some comments in the code

--- scripts/herbariumdownloader.py
+++ scripts/herbariumdownloader.py
@@ -1,11 +1,11 @@
 import argparse
 import pandas as pd
 
-
-parser = argparse.ArgumentParser(description='Process a CSV file and download images listed in it.')
+# Command-line argument parsing
+parser = argparse.ArgumentParser(description='Randomly sample records from a CSV file and download images listed in it.')
 parser.add_argument('--input', help='path to the input CSV file')
-parser.add_argument('--output', help='path to a directory to write the downloaded images to', default='')
-parser.add_argument('--samples', help='set number of randomly picked rows from the CSV file', default=1)
+parser.add_argument('--output', help='path to a directory to write a CSV file with the sampled records and downloaded images to', default='')
+parser.add_argument('--samples', help='set number of randomly picked rows from the CSV file', default=10)
 parser.add_argument('--df', help='downsampling factor to be applied to images prior to writing to disk', default=1.0)
 args = parser.parse_args()
 
@@ -20,6 +20,8 @@ subsheet = sheet.sample(n=args.samples)
 
 
 # Write the selected records to a CSV file
-subsheet.to_csv(args.output + 'test.csv', index=False)
+# TODO: Add check for validity of args.output path string
+subsheet.to_csv(args.output + 'sample.csv', index=False)
 
+# TODO: Walk through sampled records and download images, downsample and save to disk
 
