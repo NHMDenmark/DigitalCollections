ae2217fbd6b1ecfb8df716ebe59d4c5ee0694744
Kim <kimstp@di.ku.dk>
2022-03-23 22:57:58 +0100

    A working version of the herbarium downloader.

--- scripts/herbariumdownloader.py
+++ scripts/herbariumdownloader.py
@@ -10,7 +10,7 @@ from skimage.util import img_as_ubyte
 # Command-line argument parsing
 parser = argparse.ArgumentParser(description='Randomly sample records from a CSV file and download images listed in it.')
 parser.add_argument('--input', help='path to the input CSV file')
-parser.add_argument('--output', help='path to a directory to write a CSV file with the sampled records and downloaded images to', default='')
+parser.add_argument('--output', help='path to a directory to write a CSV file with the sampled records and downloaded images to', default='.')
 parser.add_argument('--samples', help='set number of randomly picked rows from the CSV file', type=int, default=10)
 parser.add_argument('--df', help='downsampling factor to be applied to images prior to writing to disk', type=float, default=1.0)
 parser.add_argument('--Q', help='set the JPEG quality parameter for processed images', type=int, default=50)
@@ -29,23 +29,28 @@ subsheet = sheet.sample(n=args.samples)
 subsheet.to_csv(PurePath(args.output, 'samples.csv'), index=False)
 
 # Walk through sampled records and download images, downsample and save to disk
-for url in subsheet["1,111-collectionobjectattachments.collectionobject.collectionObjectAttachments"]:
+for index, record in subsheet.iterrows():
+    url = record["1,111-collectionobjectattachments.collectionobject.collectionObjectAttachments"]
+    catnum = record["1.collectionobject.catalogNumber"]
+    print("Catalogue number NHMD-" + str(catnum))
     print("Download " + url)
     try:
         # TODO: Make a urllib.request based downloader instead of assuming wget is installed in the OS
-        # TODO: Set the download path to be identical to the output path
-        retval = subprocess.run(["wget " + str(url)], shell=True, check=True)
+        # Set the download path to be identical to the output path
+        retval = subprocess.run(["wget -P" + PurePath(args.output).as_posix() + " " + str(url)], shell=True, check=True)
         #print(retval)
         
         # Read image, downsample and write back to disk using NHMD catalogue number as file name. Set JPEG compression factor
-        imgfilename = PurePath(urlparse(url).path).name
-        img = imread(imgfilename)
+        imgfilename = PurePath(urlparse(url).path).name # Strip filename from URL
+        img = imread(PurePath(args.output,imgfilename).as_posix())
         
         # Downsample by a factor of args.df using nearest neighbor interpolation and anti aliasing by Gaussian filtering prior to sampling
         imgrescaled = img_as_ubyte(rescale(img, args.df, order=0, multichannel=True, anti_aliasing=True))
         
-        # TODO Set output file name to NHMD catalogue number
-        imsave(PurePath(args.output, 'samples.jpg').as_posix(), imgrescaled, quality=args.Q)
+        # Set output file name to NHMD catalogue number
+        outfilename = "NHMD-" + str(catnum) + ".jpg"
+        print("Saving image " + outfilename)
+        imsave(PurePath(args.output, outfilename).as_posix(), imgrescaled, quality=args.Q)
         
         # TODO: Delete the original downloaded file
         
